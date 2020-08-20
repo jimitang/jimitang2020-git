d02182fd312864e0e19281ae76c46f5a599bae0f
tengyue lai <tengyuelai@shanyao.local>
2020-08-20 10:02:21 +0800

    https://github.com/jimitang/jimitang2020-git.git

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+message="Hello Python world!"
+print(message)
+
+message="Hello Python Crash Course world!"
+print(message)
+
+message="Hello ’Pytho‘ Crash “Course” world!"
+print(message)
+
+message='Hello Pytho Crash "Course" world'
+print(message)
